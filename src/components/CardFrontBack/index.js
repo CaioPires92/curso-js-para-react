d6c7bf03e6f97d0fe40770614a853191ecd3d164
Caio Pires <caiogodoi_92@hotmail.com>
2022-02-13 23:48:07 -0300

    :construction: Create CardFrontBack component without CSS

---
+++ src/components/CardFrontBack/index.js
@@ -0,0 +1,14 @@
+import CardGame from "../CardGame"
+
+function CardFrontBack() {
+  return `
+
+      <article class="card-front-back">
+        ${CardGame()}
+      </article>
+      
+
+  `
+}
+
+export default CardFrontBack
