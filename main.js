06cb1241569b1838c7067a401f2b55a8e241c243
Caio Pires <caiogodoi_92@hotmail.com>
2022-02-18 18:56:55 -0300

    :construction: Create component ArrowDown without CSS

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const $root = document.querySelector('#root')
 
 $root.insertAdjacentHTML(
   'beforeend',
-
-  `${ScoreBoard()}
-    ${BoardGame(6)}
+  ` 
+  ${ScoreBoard()}
+  ${BoardGame(6)}
     `
 )
---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,9 @@
+function ArrowDown() {
+  return /*html*/ `
+
+  <img src="images/ArrowDown.png" alt="Icone de uma seta para baixo"/>
+
+  `
+}
+
+export default ArrowDown
